379b759945bc6384fadedf1f4fca15fa7344675b
xCristinaS <cristina.sola.1992@gmail.com>
2015-10-29 13:33:15 +0100

    minipractica botonera

---
+++ 05_MiniPrac_Botonera/05_MiniPrac_Botonera/05_MiniPrac_Botonera/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace _05_MiniPrac_Botonera {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.btnPosterior = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblNombreMostrar = new System.Windows.Forms.Label();
+            this.btnModo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(27, 26);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(80, 23);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Location = new System.Drawing.Point(12, 158);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(75, 23);
+            this.btnAnterior.TabIndex = 2;
+            this.btnAnterior.Text = "Anterior";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            this.btnAnterior.Click += new System.EventHandler(this.mostrarAnterior);
+            // 
+            // btnPosterior
+            // 
+            this.btnPosterior.Location = new System.Drawing.Point(93, 158);
+            this.btnPosterior.Name = "btnPosterior";
+            this.btnPosterior.Size = new System.Drawing.Size(75, 23);
+            this.btnPosterior.TabIndex = 3;
+            this.btnPosterior.Text = "Posterior";
+            this.btnPosterior.UseVisualStyleBackColor = true;
+            this.btnPosterior.Click += new System.EventHandler(this.mostrarPosterior);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(174, 158);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 4;
+            this.btnNuevo.Text = "limpiar";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.limpiar);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(12, 187);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.cancelar);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(93, 187);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 6;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.modificar);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(174, 187);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 7;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.guardar);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(12, 216);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.eliminar);
+            // 
+            // lblNombreMostrar
+            // 
+            this.lblNombreMostrar.AutoSize = true;
+            this.lblNombreMostrar.Location = new System.Drawing.Point(77, 26);
+            this.lblNombreMostrar.Name = "lblNombreMostrar";
+            this.lblNombreMostrar.Size = new System.Drawing.Size(0, 13);
+            this.lblNombreMostrar.TabIndex = 9;
+            // 
+            // btnModo
+            // 
+            this.btnModo.Location = new System.Drawing.Point(80, 72);
+            this.btnModo.Name = "btnModo";
+            this.btnModo.Size = new System.Drawing.Size(100, 23);
+            this.btnModo.TabIndex = 10;
+            this.btnModo.UseVisualStyleBackColor = true;
+            this.btnModo.Click += new System.EventHandler(this.cambiarModo);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnModo);
+            this.Controls.Add(this.lblNombreMostrar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnPosterior);
+            this.Controls.Add(this.btnAnterior);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.Button btnPosterior;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblNombreMostrar;
+        private System.Windows.Forms.Button btnModo;
+    }
+}
+
---
+++ 05_MiniPrac_Botonera/05_MiniPrac_Botonera/05_MiniPrac_Botonera/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05_MiniPrac_Botonera {
+    public partial class Form1 : Form {
+
+        private ArrayList nombres = new ArrayList();
+        private const string NUEVO = "Agregar Nuevo", VI = "Modo Visión";
+        private string nombreSelec; bool modify = false;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            lblNombreMostrar.Hide();
+            btnModo.Hide();
+            btnAnterior.Hide();
+            btnPosterior.Hide();
+            btnEliminar.Hide();
+            btnModificar.Hide();
+            btnCancelar.Hide();
+        }
+
+        private void cambiarModo(object sender, EventArgs e) {
+            if (nombres.Count > 0) {
+                if (!btnModo.Text.Equals(NUEVO)) {
+                    modoVisionActivado();
+                    btnModo.Text = NUEVO;
+                } else {
+                    modoEdicionActivado();
+                    btnModo.Text = VI;
+                }
+                modify = false;
+            }
+        }
+
+        private void modoVisionActivado() {
+            btnAnterior.Show();
+            btnPosterior.Show();
+            btnEliminar.Show();
+            btnModificar.Show();
+            btnCancelar.Hide();
+            btnNuevo.Hide();
+            btnGuardar.Hide();
+            lblNombreMostrar.Show();
+            txtNombre.Hide();
+            if (nombres.Count > 0) {
+                nombreSelec = (string)nombres[0];
+                lblNombreMostrar.Text = nombreSelec;
+            }
+        }
+
+        private void modoEdicionActivado() {
+            btnAnterior.Hide();
+            btnPosterior.Hide();
+            btnEliminar.Hide();
+            btnModificar.Hide();
+            btnCancelar.Show();
+            btnNuevo.Show();
+            btnGuardar.Show();
+            lblNombreMostrar.Hide();
+            txtNombre.Show();
+            if (nombres.Count > 0)
+                btnCancelar.Show();
+            else
+                btnCancelar.Hide();
+        }
+
+        private void nuevo(object sender, EventArgs e) {
+            txtNombre.Text = "";
+        }
+
+        private void mostrarAnterior(object sender, EventArgs e) {
+            int i;
+            if ((i = nombres.IndexOf(nombreSelec)) > 0) {
+                nombreSelec = (string)nombres[i - 1];
+                lblNombreMostrar.Text = nombreSelec;
+            }
+        }
+
+        private void mostrarPosterior(object sender, EventArgs e) {
+            int i;
+            if ((i = nombres.IndexOf(nombreSelec)) < nombres.Count - 1) {
+                nombreSelec = (string)nombres[i + 1];
+                lblNombreMostrar.Text = nombreSelec;
+            }
+        }
+
+        private void cancelar(object sender, EventArgs e) {
+            modoVisionActivado();
+            modify = false;
+        }
+        // Pensar el modificar. 
+        private void modificar(object sender, EventArgs e) {
+            modoEdicionActivado();
+            txtNombre.Text = nombreSelec;
+            btnModo.Text = VI;
+            modify = true;
+        }
+
+        private void eliminar(object sender, EventArgs e) {
+            if (nombres.Count > 0) {
+                nombres.Remove(nombreSelec);
+                if (nombres.Count > 0) {
+                    nombreSelec = (string)nombres[0];
+                    lblNombreMostrar.Text = nombreSelec;
+                } else {
+                    modoEdicionActivado();
+                    btnModo.Hide();
+                }
+            } 
+        }
+
+        private void limpiar(object sender, EventArgs e) {
+            txtNombre.Text = "";
+        }
+
+        private void guardar(object sender, EventArgs e) {
+            if (nombres.Count == 0) {
+                btnModo.Show();
+                btnModo.Text = VI;
+            }
+
+            if (!modify)
+                nombres.Add(txtNombre.Text);
+            else {
+                nombres[nombres.IndexOf(nombreSelec)] = txtNombre.Text;
+            }
+            txtNombre.Text = "";
+        }
+
+    }
+}
